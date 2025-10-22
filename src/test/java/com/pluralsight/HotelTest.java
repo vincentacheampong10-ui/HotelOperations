7d44886507048931038172dafb40e42aeabf547b
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-10-22 14:24:41 -0400

    hotel test

--- src/test/java/com/pluralsight/HotelTest.java
+++ src/test/java/com/pluralsight/HotelTest.java
@@ -2,26 +2,27 @@ package com.pluralsight;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HotelTest {
 
     @Test
-    void bookRoom_whenRoomsAvailabel_ReturnTrue() {
+    void bookRoom_whenSuitesAvailabel_ReturnTrue() {
         // arrange
         Hotel grandBudapestHotel = new Hotel("Grand Budapest Hotel", 15, 100);
         // act: call method
-       boolean result= grandBudapestHotel.bookRoom (5, false);
+       boolean result= grandBudapestHotel.bookRoom (5, true);
        //assert: verify changes
         assertTrue(result);
     }
     @Test
     void bookRoom_whenRoomsAreNotAvailabel_ReturnFalse() {
         // arrange
-        Hotel grandBudapestHotel = new Hotel("Grand Budapest Hotel", 15, 101);
+        Hotel grandBudapestHotel = new Hotel("Grand Budapest Hotel", 15, 100);
         // act: call method
-        boolean result = grandBudapestHotel.bookRoom (5, false);
+        boolean result = grandBudapestHotel.bookRoom (101, true);
         //assert: verify changes
         assertTrue(result);
     }
-}
+    }
